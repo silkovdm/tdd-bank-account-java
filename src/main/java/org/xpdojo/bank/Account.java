9e8d814634106e31d3fc08236b59bbe8e2515e17
Dmitry Silkov <silkov@mail.ru>
2020-07-16 17:08:34 +0300

    Tests

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,8 +1,33 @@
 package org.xpdojo.bank;
 
+import javax.management.relation.RoleUnresolved;
+
 public class Account {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private int balance = 0;
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        if (balance - amount > 0) {
+            this.balance -= amount;
+        } else {
+            throw new RuntimeException("Balance " + balance + "is insufficient to withdraw " + amount);
+        }
+    }
+
+    public void transfer(int amount, Account accountTo) {
+        if (balance - amount > 0) {
+            this.withdraw(amount);
+            accountTo.deposit(amount);
+        } else {
+            throw new RuntimeException("Balance " + balance + "is insufficient to transfer " + amount);
+        }
     }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,15 +1,57 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
 
     @Test
-	@Disabled
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        assertEquals(0, account.getBalance());
+
+        account.deposit(10);
+        assertEquals(10, account.getBalance());
+    }
+
+    @Test
+    public void withdrawAnAmountToDecreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        account.withdraw(9);
+
+        assertEquals(1, account.getBalance());
+    }
+
+    @Test
+    public void withdrawAnAmountMoreThanBalance() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            Account account = new Account();
+            account.withdraw(10);
+        });
+    }
+
+    @Test
+    public void transferBetweenAccounts() {
+        Account account1 = new Account();
+        Account account2 = new Account();
+
+        account1.deposit(10);
+        account1.transfer(8, account2);
+
+        assertEquals(2, account1.getBalance());
+        assertEquals(8, account2.getBalance());
+    }
+
+    @Test
+    public void transferBetweenAccountsMoreThanBalance() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            Account account1 = new Account();
+            Account account2 = new Account();
+
+            account1.transfer(8, account2);
+        });
     }
 }
